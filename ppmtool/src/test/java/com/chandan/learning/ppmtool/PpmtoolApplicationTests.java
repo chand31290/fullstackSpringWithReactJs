8dbf3461526c25c7a60951f4542e3fab554b03dc
Chandan Singh <chand31290@gmail.com>
2020-10-11 13:24:43 +0530

    backend skeleton project first commit

---
+++ ppmtool/src/test/java/com/chandan/learning/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandan.learning.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
